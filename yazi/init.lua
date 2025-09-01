60d1c607e56b54e0a53c8c50fc9afd1882cccad0
Thysrael <thysrael@163.com>
2025-09-01 17:23:56 +0800

    feat(yazi): polish yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -7,3 +7,28 @@ Status:children_add(function(self)
 		return ""
 	end
 end, 3300, Status.LEFT)
+
+-- add symbol link in status line
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+-- add user:group in status line
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)),
+		" ",
+	}
+end, 500, Status.RIGHT)
